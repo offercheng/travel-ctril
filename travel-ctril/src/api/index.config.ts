1f182ba227fc877dbf20b3c271eb3008d6973baa
chengmaomao <2951067747@qq.com>
2022-01-06 10:55:05 +0800

    对项目的 api接口层做了封装

---
+++ travel-ctril/src/api/index.config.ts
@@ -0,0 +1,3 @@
+// 这里要根据 在不同的环境下面进行不同的 url 配置
+export const BaseURL: string = "http://localhost:5050/";
+export const TIMEOUT: number = 500;
---
+++ travel-ctril/src/api/index.ts
@@ -0,0 +1,9 @@
+// 基础的路由
+const url = 'http://localhost:5050/';
+
+// 配置路径
+const serverPath = {
+  checkLogin: url + "checkLogin",   // 登录接口检查用户名和密码
+}
+
+export default serverPath
