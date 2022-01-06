2b982151bda3865ca64def0b0a74122aa2b06583
chengmaomao <2951067747@qq.com>
2022-01-06 10:55:41 +0800

    对项目的 axios的类型做了做了封装

---
+++ travel-ctril/src/config/axios/axios.type.ts
@@ -0,0 +1,28 @@
+import { AxiosRequestConfig } from "axios";
+
+export interface AxiosResponse< T = any > {
+  data: T;
+  config: AxiosRequestConfig;
+}
+
+// 返回数据类型
+export interface IDataType<T = any> {
+  status: string
+  data: T;
+}
+
+
+export interface HYRequestInterceptors<T = AxiosResponse> {
+  // 请求成功
+  requestInterceptor?: (config: AxiosRequestConfig) => AxiosRequestConfig
+  // 请求失败
+  requestInterceptorCatch?: (error: any) => any
+  // 响应成功
+  responseInterceptor?: (res: T) => T
+  // 响应失败
+  responseInterceptorCatch?: (error: any) => any
+}
+
+export interface HYRequestConfig<T = AxiosResponse> extends AxiosRequestConfig {
+  interceptors?: HYRequestInterceptors<T>
+}
